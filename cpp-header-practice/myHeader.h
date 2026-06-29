38a82e52367710c68f71e81297084d8c54b75cf2
Mamad_RC <100014429+mohammadreza-r@users.noreply.github.com>
2026-06-29 17:23:51 +0330

    Create myHeader.h

---
+++ cpp-header-practice/myHeader.h
@@ -0,0 +1,11 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+// #include <iostream>
+#include <map>
+#include <string>
+
+bool H_isPrime(int number);
+std::map<std::string, int> dayToYMD(int daysLived);
+
+#endif
